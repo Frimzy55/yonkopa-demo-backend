001430c635d261cca282072bca5ae7f15d1b557b
Joseph Frimpong <frimpongj190@gmail.com>
2026-05-08 12:01:22 +0000

    'update'

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ const dbConfig = process.env.NODE_ENV === "development"
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      port: process.env.PORT,
+      port: process.env.DB_PORT,
     };
 
 const db = mysql.createPool({
