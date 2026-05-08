e7ae8b4b28b4f4913b91c42c0295657d61630aec
Joseph Frimpong <frimpongj190@gmail.com>
2026-05-08 11:50:54 +0000

    'update'

--- server.js
+++ server.js
@@ -103,7 +103,7 @@ app.use(cors({
 
 
 
-// ✅ Middleware
+// ✅ Middlewar
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
